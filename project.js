40f56ab4f2a69e0f93c55bcadb0a1d361cb071da
mle-gars <maxime.legars@hotmail.fr>
2024-01-29 11:54:14 +0100

    :bug: update: intersection resolved

--- project.js
+++ project.js
@@ -1,20 +1,25 @@
-const liste1 = [1, 2, 3, 4, 5];
-const liste2 = [3, 4, 5, 6, 7];
-
-function findIntersection(liste1, liste2) {
-    const intersection = [];
+document.addEventListener("DOMContentLoaded", function() {
     
-    for (const element of liste1) {
-        if (liste2.includes(element)) {
-            intersection.push(element);
+    function trouverIntersection() {
+        const liste1 = [1, 2, 3, 4, 5];
+        const liste2 = [3, 4, 5, 6, 7];
+        const intersection = [];
+
+        for (const element of liste1) {
+            if (liste2.includes(element)) {
+                intersection.push(element);
+            }
         }
+
+        console.log("Intersection:", intersection);
     }
-    
-    return intersection;
-}
 
-const intersection = findIntersection(liste1, liste2);
+    // Récupérer le bouton
+    const bouton = document.getElementById("boutonIntersection" );
 
-console.log("Liste 1:", liste1);
-console.log("Liste 2:", liste2);
-console.log("Intersection:", intersection);
+    // Ajouter un gestionnaire d'événement de clic au bouton
+    bouton.onclick = function() {
+        trouverIntersection();
+    };
+
+});
